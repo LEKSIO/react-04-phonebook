10026866c4b29add77dfc6db2d23c88e1de0df17
Oleksii Bohoslavskyi <oleks.bohos@gmail.com>
2024-02-29 23:38:55 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { nanoid } from 'nanoid';
 import {
   ContactsList,
@@ -7,12 +7,28 @@ import {
   ContactsListFilter,
 } from 'components';
 
+const STORAGE_KEY = 'Contacts_array';
+
 export const App = () => {
   const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
+  const isFirstRenderRef = useRef(true);
   const filteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(filter.trim().toLowerCase())
   ); // (Note: any string includes an empty string)
+
+  useEffect(() => {
+    const storageContacts = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    storageContacts && setContacts(storageContacts);
+  }, []);
+
+  useEffect(() => {
+    if (!isFirstRenderRef.current)
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(contacts));
+    return () => {
+      isFirstRenderRef.current = false;
+    };
+  }, [contacts]);
 
   const addContact = formDataObj => {
     const hasDuplicate = contacts.some(
